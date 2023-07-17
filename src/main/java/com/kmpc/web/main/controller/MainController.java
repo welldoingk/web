4d677d911966872ee92ff31baca4de2fef19faaa
welldoingk <thkwon96@gmail.com>
2023-07-18 08:36:26 +0900

    Initial commit

---
+++ src/main/java/com/kmpc/web/main/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.kmpc.web.main.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+
+//    @RequestMapping({"", "/"})
+//    public String index(){
+//        return "pages/test/test";
+//    }
+
+    @RequestMapping({"2"})
+    public String index2(){
+        return "pages/indexO";
+    }
+}

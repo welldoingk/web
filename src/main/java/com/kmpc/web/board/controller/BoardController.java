fbf63ce624c556b93e7e7cc9e66ff43cc355df89
welldoingk <thkwon96@gmail.com>
2023-07-25 09:52:53 +0900

    board

--- src/main/java/com/kmpc/web/board/controller/BoardController.java
+++ src/main/java/com/kmpc/web/board/controller/BoardController.java
@@ -1,10 +1,14 @@
 package com.kmpc.web.board.controller;
 
 import com.kmpc.web.board.dto.BoardDto;
+import com.kmpc.web.board.repository.CustomBoardRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 
@@ -13,13 +17,16 @@ import org.springframework.web.bind.annotation.PatchMapping;
 public class BoardController {
 
     @Autowired
+    CustomBoardRepository customBoardRepository;
 
     @GetMapping("/board")
-    public String list() {
-//        Page<BoardDto> results = customBoardRepository.selectBoardList(searchVal, pageable);
-//        model.addAttribute("list", results);
-//        model.addAttribute("maxPage", 5);
-//        pageModelPut(results, model);
+    public String list(String searchVal, Pageable pageable, Model model) {
+        Page<BoardDto> results = customBoardRepository.selectBoardList(searchVal, pageable);
+        model.addAttribute("list", results);
+        model.addAttribute("maxPage", 5);
+        model.addAttribute("totalCount", results.getTotalElements());
+        model.addAttribute("size", results.getPageable().getPageSize());
+        model.addAttribute("number", results.getPageable().getPageNumber());
         return "pages/board/list2";
     }
 
@@ -32,6 +39,5 @@ public class BoardController {
     public String update() {
         return "pages/board/update";
     }
-
 
 }
